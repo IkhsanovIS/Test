5948e5a1d351a1f84b0b70aeb3580e54c3adfa83
Ильср <@DESKTOP-1PS2I6N>
2016-12-10 23:51:22 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Category
+    {
+        //private string name;
+        public Category(string name)
+        {
+            if (QuerySQL.Query(name) == null)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Email.Dispatch(name);
+            }
+        }
+
+    }
+    class QuerySQL
+    {
+        public static string Query(string name)
+        {
+            return "результат запроса";
+        }
+    }
+    class Email
+    {
+        public static void Dispatch(string name)
+        {
+            Console.WriteLine("Создана категория " + name); //отправка email
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Category category = new Category("Тест");
+
+        }
+    }
+}
